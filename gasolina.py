8b03a3e7d45908b2035db491c2705107fab5d419
RFlores78 <r.flores788@gmail.com>
2024-05-22 17:29:52 +0000

    Adiciona arquivos de dados, código Python e gráfico de preços da gasolina

---
+++ gasolina.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+def main():
+    df = pd.read_csv('gasolina.csv')
+    df.rename(columns={'dia': 'Dia', 'venda': 'Preço'}, inplace=True)
+    print(df.head())
+
+if __name__ == "__main__":
+    main()
